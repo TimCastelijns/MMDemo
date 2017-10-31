ad0fa7469fec0c7a843973bc3c4d7fe7ea4477bf
Tim Castelijns <timcastelijns@gmail.com>
2017-10-31 14:31:10 +0100

    Replace template navigation items

--- app/src/main/java/com/castelijns/mmdemo/MainActivity.java
+++ app/src/main/java/com/castelijns/mmdemo/MainActivity.java
@@ -17,14 +17,14 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_albums:
+                    mTextMessage.setText(R.string.title_albums);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_photos:
+                    mTextMessage.setText(R.string.title_photos);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_users:
+                    mTextMessage.setText(R.string.title_users);
                     return true;
             }
             return false;
@@ -36,8 +36,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = findViewById(R.id.message);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
